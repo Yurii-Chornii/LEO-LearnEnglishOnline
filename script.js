ec705f8c3cfc8abd56290106e9ad5cbbf579dd5b
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-11-24 00:31:26 +0200

    done bug fixed 3 (youtube onload)

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//youtube
+ window.onload = () => {
+    document.querySelector('#whyUs-videoBox').innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/a9cSMaGJ5MM" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>`;
+     document.querySelector('#howAreTheClasses div').innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/G4hbyoojIrM" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>`;
+ }
+
  //smoothScroll
     let linkNav = document.querySelectorAll('[href^="#"]'), V = 0.2;
     for (let i = 0; i < linkNav.length; i++) {
